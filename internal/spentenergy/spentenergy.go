c579036a8f3771b563685ee8d8f2d66bff73be65
Vladimir Lakkama <alkolost@gmail.com>
2025-05-31 14:38:27 +0300

    first fail. Base variant

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,9 +1,6 @@
 package spentenergy
 
-import (
-	"errors"
-	"time"
-)
+import "time"
 
 // Основные константы, необходимые для расчетов.
 const (
@@ -13,61 +10,23 @@ const (
 	walkingCaloriesCoefficient = 0.5  // коэффициент для расчета калорий при ходьбе.
 )
 
-// WalkingSpentCalories для расчета калорий при ходьбе
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
-	switch {
-	case steps <= 0:
-		return 0, errors.New("Value 'steps' must be positive")
-	case weight <= 0:
-		return 0, errors.New("Value 'weight' must be positive")
-	case height <= 0:
-		return 0, errors.New("Value 'height' must be positive")
-	case duration <= 0:
-		return 0, errors.New("Value 'duration' must be positive")
-	}
-
-	meanSpeed := MeanSpeed(steps, height, duration)
-	return ((weight * meanSpeed * duration.Minutes()) / minInH) * walkingCaloriesCoefficient, nil
 
 }
 
 // RunSpentCalories для расчета калорий при беге
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
-	switch {
-	case steps <= 0:
-		return 0, errors.New("Value 'steps' must be positive")
-	case weight <= 0:
-		return 0, errors.New("Value 'weight' must be positive")
-	case height <= 0:
-		return 0, errors.New("Value 'height' must be positive")
-	case duration <= 0:
-		return 0, errors.New("Value 'duration' must be positive")
-	}
 
-	meanSpeed := MeanSpeed(steps, height, duration)
-	return (weight * meanSpeed * duration.Minutes()) / minInH, nil
 }
 
-// MeanSpeed считает среднюю скорость
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
 	// TODO: реализовать функцию
-	if steps <= 0 || height <= 0 || duration <= 0 {
-		return 0
-	}
-	return Distance(steps, height) / duration.Hours()
 
 }
 
-// Distance считает дистанцию в км
 func Distance(steps int, height float64) float64 {
 	// TODO: реализовать функцию
-	if steps <= 0 || height <= 0 {
-		return 0
-	}
-	length := height * stepLengthCoefficient
-	dist := float64(steps) * length
-	distKm := dist / mInKm
-	return distKm
+
 }
--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,96 +1,13 @@
 package trainings
 
-import (
-	"errors"
-	"fmt"
-	"log"
-	"strconv"
-	"strings"
-	"time"
-
-	"github.com/Yandex-Practicum/tracker/internal/personaldata"
-	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
-)
-
 type Training struct {
 	// TODO: добавить поля
-	personaldata.Personal
-	Steps        int
-	TrainingType string
-	Duration     time.Duration
 }
 
 func (t *Training) Parse(datastring string) (err error) {
 	// TODO: реализовать функцию
-	parts := strings.Split(datastring, ",")
-	if len(parts) != 3 {
-		err := errors.New("invalid input format: expected 'steps, training type, duration'")
-		log.Printf("datastring error: %v", err)
-		return err
-	}
-	// Преобразование шагов
-	stepStr := strings.TrimSpace(parts[0])
-	steps, err := strconv.Atoi(stepStr)
-
-	if err != nil {
-		err := errors.New("invalid steps format: " + err.Error())
-		log.Printf("datastring error: %v", err)
-		return err
-	}
-
-	if steps <= 0 {
-		err := errors.New("steps must be positive")
-		log.Printf("steps must be positive: %v", err)
-		return err
-	}
-
-	t.Steps = steps
-	// Тип тренировки
-	trainingType := strings.TrimSpace(parts[1])
-	if trainingType == "" {
-		log.Printf("training type is empty: %v", err)
-		return errors.New("training type can't be empty")
-	}
-	t.TrainingType = trainingType
-
-	//Время тренировки
-	durationStr := strings.TrimSpace(parts[2])
-	duration, err := time.ParseDuration(durationStr)
-	if err != nil {
-		log.Printf("ParseDuration error: %v", err)
-		return errors.New("invalid duration format: " + err.Error())
-	}
-	if duration <= 0 {
-		log.Printf("Durarition not positive: %v", err)
-		return errors.New("duration must be positive")
-	}
-
-	t.Duration = duration
-	return nil
 }
 
 func (t Training) ActionInfo() (string, error) {
 	// TODO: реализовать функцию
-	distance := spentenergy.Distance(t.Steps, t.Personal.Height)
-	meanSpeed := spentenergy.MeanSpeed(t.Steps, t.Personal.Height, t.Duration)
-	energyRun, err := spentenergy.RunningSpentCalories(t.Steps, t.Personal.Height, t.Personal.Weight, t.Duration)
-	if err != nil {
-		log.Printf("energyRun error: %v", err)
-		return "", fmt.Errorf("Error in RunningSpentCalories: %w", err)
-	}
-	energyWalk, err := spentenergy.WalkingSpentCalories(t.Steps, t.Personal.Height, t.Personal.Weight, t.Duration)
-	if err != nil {
-		log.Printf("energyRun error: %v", err)
-		return "", fmt.Errorf("Error in WalkingSpentCalories: %w", err)
-	}
-
-	if t.TrainingType == "Бег" {
-		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", t.TrainingType, t.Duration.Hours(), distance, meanSpeed, energyRun), nil
-	}
-	if t.TrainingType == "Ходьба" {
-		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", t.TrainingType, t.Duration.Hours(), distance, meanSpeed, energyWalk), nil
-	}
-	log.Printf("training type error: %v", err)
-	return "", errors.New("неизвестный тип тренировки")
-
 }
